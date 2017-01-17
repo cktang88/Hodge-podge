d515f9f5339e07c57143bb770d58b60160007ae9
GreenLantern101 <arctangent20@yahoo.com>
2017-01-16 23:08:40 -0600

    Add autoplay and hide controls

--- FixStep/public/js/main.js
+++ FixStep/public/js/main.js
@@ -1,36 +1,84 @@
+
+
+
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
+
+document.onload = function(){
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  onYouTubeIframeAPIReady();
+  alert(player);
+}
+
 function onYouTubeIframeAPIReady() {
     alert(player);
-    player = new YT.Player('player', {
+    player = new YT.Player('iframe_player', {
         height: '640',
         width: '960',
         videoId: 'AYRb9WqYEzA',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0
+        },
         events: {
             'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
+            'onStateChange': onPlayerStateChange,
+            'onError': onPlayerError
         }
     });
 }
+
+
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
+
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
 var done = false;
 
-function onPlayerStateChange(event) {
+function changeBorderColor(playerStatus) {
+    var color;
+    if (playerStatus == -1) {
+        color = "#37474F"; // unstarted = gray
+    } else if (playerStatus == 0) {
+        color = "#FFFF00"; // ended = yellow
+    } else if (playerStatus == 1) {
+        color = "#33691E"; // playing = green
+    } else if (playerStatus == 2) {
+        color = "#DD2C00"; // paused = red
+    } else if (playerStatus == 3) {
+        color = "#AA00FF"; // buffering = purple
+    } else if (playerStatus == 5) {
+        color = "#FF6DOO"; // video cued = orange
+    }
+    if (color) {
+        document.getElementById('existing-iframe-example').style.borderColor = color;
+    }
+}
 
+function onPlayerStateChange(event) {
+    changeBorderColor(event.data);
+}
+/*
+function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
         setTimeout(stopVideo, 6000);
         done = true;
     }
 }
+*/
 
 function stopVideo() {
     player.stopVideo();
