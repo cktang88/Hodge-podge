8665970870da927e7b6bdecbeb9f81bbe4602ae7
GreenLantern101 <arctangent20@yahoo.com>
2017-05-29 12:07:45 -0500

    Find user events, refactor

--- gh-user-email/server.js
+++ gh-user-email/server.js
@@ -3,16 +3,12 @@ const cheerio = require('cheerio');
 const fs = require('fs')
 
 var makeurl = function (name) {
-    var s = 'https://api.github.com/users/'+name;
+    var s = 'https://api.github.com/users/' + name;
     return s;
 }
-//find user email
-// go to /users/ api page --> get as json
-// go find their list of repos/events?
-// go get their commit, find email there
-var findemail = function (user) {
+var fetch = function (_url, callback) {
     var options = {
-        url: makeurl(user),
+        url: _url,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
         }
@@ -20,29 +16,55 @@ var findemail = function (user) {
     request(options, function (err, res, body) {
         if (err) {
             console.log(err);
-            return;
+            return callback(null);
         }
         // some websites send 200 codes to bots to fool bots
         if (res.statusCode !== 200) {
             console.log('res status code: ', res.statusCode);
             console.log(body);
+            return callback(null);
+        }
+        return callback(body);
+    });
+}
+//find user email
+var findemail = function (username) {
+
+    // option1: try to get from /users/[username] page directly, most likely to fail
+    fetch(makeurl(username), function (body) {
+        var user = JSON.parse(body);
+        if (user.email) {
+            console.log(user.email);
             return;
         }
-        var user = JSON.parse(body);
+        //default
+        console.log('Email for \'' + username + '\' could not be found via option #1.');
+    });
+
+    // option2: go to /users/[username]/events page to get events, then get commits, then get email
+    fetch(makeurl(username) + '/events', function(body){
+        var events = JSON.parse(body);
+        var pushevents = [];
+        console.log(events.length + ' events found.');
+        for (var i = 0; i < events.length; i++) {
+            var e = events[i];
+            if (e.type === 'PushEvent')
+                pushevents.push(e);
+        }
+        console.log(pushevents.length);
         /*
-        fs.writeFile('data/data.json', body, function (err) {
+        fs.writeFile('data/data.json', events, function (err) {
             if (err)
                 console.log(err);
             else
                 console.log('successfully written to file.');
         });
         */
-        console.log(user.email);
-
+        //console.log(events);
     });
 }
 
 function main() {
-    findemail('GreenLantern101')
+    findemail('sindresorhus');
 }
 main()
