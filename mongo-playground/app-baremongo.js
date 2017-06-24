c77fd46995680eba69c914056afca7ae743f738a
GreenLantern101 <arctangent20@yahoo.com>
2017-06-24 13:59:49 -0500

    Promisify all CRUD ops

--- mongo-playground/app-baremongo.js
+++ mongo-playground/app-baremongo.js
@@ -1,8 +1,12 @@
 // do bare mongo first, use Monk later.
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 const Promise = require('bluebird');
+var mongo = require('mongodb');
+// promisify
+Promise.promisifyAll(mongo);
+
+const MongoClient = mongo.MongoClient;
+const assert = require('assert');
+
 
 // Connection 
 const user = process.env.user;
@@ -11,83 +15,78 @@ const dbname = process.env.db_name;
 const url = `mongodb://${user}:${password}@ds135382.mlab.com:35382/${dbname}`;
 console.log(url);
 // Use connect method to connect to the Server
-MongoClient.connect(url, (err, db) => {
-	assert.equal(null, err);
-	console.log(`Connected correctly to server at ${url}`);
-	// clear database
-	db.dropDatabase();
-	console.log('Database cleared.');
+MongoClient.connectAsync(url)
+	.then(function (db) {
+		//assert.equal(null, err);
+		console.log(`Connected correctly to server at ${url}`);
+		// clear database
+		db.dropDatabase();
+		console.log('Database cleared.');
 
-	// lol callback hell --> TODO: use promises
-	insertDocuments(db, () => {
-		updateDocument(db, () => {
-			deleteDocument(db, () => {
-				findDocuments(db, () => {
-					db.close();
-				});
-			});
-		});
-	});
+		// Get the documents collection
+		return db.collection('documents');
+		// "col" = collection
+	}).then(function (col) {
+		insertDocuments(col);
+		console.log("Inserted 3 docs into 'documents' collection.");
+		return col;
+	})
+	.then(function (col) {
+		updateDocument(col);
+		console.log("Updated doc where a = 2.");
+		return col;
+	})
+	.then(function (col) {
+		deleteDocument(col);
+		console.log("Removed doc where a = 3.");
+		return col;
+	})
+	.then(function (col) {
+		return findDocuments(col);
+	})
+	.then(function(docs){
+		return docs.toArrayAsync();
+	})
+	.then(function (docs) {
+		console.log("Found the following records:");
+		console.log(docs);
+	})
+	.catch(function (err) {
+		console.log(err);
+	})
+/*
+.finally(function (db) {
+	db.close();
 });
+*/
 
-let insertDocuments = (db, callback) => {
-	// Get the documents collection
-	var collection = db.collection('documents');
+let insertDocuments = (col) => {
 	// Insert some documents
-	collection.insertMany([{
+	return col.insertManyAsync([{
 		a: 1
 	}, {
 		a: 2
 	}, {
 		a: 3
-	}], function (err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
-		console.log("Inserted 3 documents into the document collection");
-		callback(result);
-	});
+	}]);
 }
-let updateDocument = (db, callback) => {
-	// Get the documents collection
-	var collection = db.collection('documents');
+let updateDocument = (col) => {
 	// Update document where a is 2, set b equal to 1
-	collection.updateOne({
+	return col.updateOneAsync({
 		a: 2
 	}, {
 		$set: {
 			b: 1
 		}
-	}, function (err, result) {
-		assert.equal(err, null);
-		assert.equal(1, result.result.n);
-		console.log("Updated the document with the field a equal to 2");
-		callback(result);
 	});
 }
 
-let deleteDocument = (db, callback) => {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Insert some documents
-	collection.deleteOne({
+let deleteDocument = (col) => {
+	// delete doc
+	return col.deleteOneAsync({
 		a: 3
-	}, function (err, result) {
-		assert.equal(err, null);
-		assert.equal(1, result.result.n);
-		console.log("Removed the document with the field a equal to 3");
-		callback(result);
 	});
 }
-let findDocuments = (db, callback) => {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Find some documents
-	collection.find({}).toArray(function (err, docs) {
-		assert.equal(err, null);
-		assert.equal(2, docs.length);
-		console.log("Found the following records");
-		console.dir(docs);
-		callback(docs);
-	});
+let findDocuments = (col) => {
+	return col.findAsync({});
 }
